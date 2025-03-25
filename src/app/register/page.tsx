ea9820e1e94071cb9431bc210339b4b055b92127
Jukkruu <outanagon2549@gmail.com>
2025-03-25 15:09:22 +0700

    UI1

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,143 +1,137 @@
-"use client"
+"use client";
 import DateReserve from "@/components/DateReserve";
-import { TextField, Button, Select, MenuItem } from "@mui/material"
+import { TextField, Button, InputAdornment, IconButton } from "@mui/material";
 import { useState } from "react";
-import dayjs, { Dayjs } from "dayjs";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "@/redux/store";
-import { addBooking } from "@/redux/features/bookSlice";
-import React from "react";
-
-import { SelectChangeEvent } from "@mui/material/Select";
-import Company from "@/db/models/Company"
-import { dbConnect } from "@/db/dbConnect"
-import { revalidateTag } from "next/cache"
-import { redirect } from "next/navigation"
-import userRegister from "@/libs/userRegister";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
-import {IconButton, InputAdornment } from '@mui/material';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
+import userRegister from "@/libs/userRegister";
+
+export default function Booking() {
+  const router = useRouter();
+
+  const [name, setName] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [password2, setPassword2] = useState("");
+  const [showPassword2, setShowPassword2] = useState(false);
+
+  const validateEmail = (email: string) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const validatePhoneNumber = (phone: string) => {
+    const regex = /^0\d{9}$/;
+    return regex.test(phone);
+  };
+
+  const checkPassword = (
+    password1: string,
+    password2: string,
+    email: string,
+    name: string,
+    contactNumber: string
+  ) => {
+    if (!validatePhoneNumber(contactNumber)) {
+      alert("Please enter a valid phone number (10 digits and starts with 0)");
+      return false;
+    }
+    if (!validateEmail(email)) {
+      alert("Please enter a valid email address");
+      return false;
+    }
+
+    if (password1 !== password2) {
+      alert("Passwords do not match. Please try again.");
+      return false;
+    }
+
+    userRegister(email, password2, name, contactNumber);
+    router.push("/");
+    return true;
+  };
+
+  return (
+    <main className="w-full flex flex-col items-center py-10 bg-gradient-to-r from-blue-200 to-purple-300">
+      <div className="text-3xl font-bold text-stone-950 mb-8">Create Your Account</div>
+
+      <div className="space-y-6 p-8 flex flex-col max-w-lg mx-auto bg-white shadow-xl rounded-lg">
+        <TextField
+          variant="outlined"
+          name="Name-Lastname"
+          label="Name-Lastname"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          fullWidth
+        />
+
+        <TextField
+          variant="outlined"
+          name="Contact-Number"
+          label="Contact Number"
+          value={contactNumber}
+          onChange={(e) => setContactNumber(e.target.value)}
+          fullWidth
+        />
+
+        <TextField
+          variant="outlined"
+          name="email"
+          label="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          fullWidth
+        />
+
+        <TextField
+          variant="outlined"
+          name="password"
+          label="Password"
+          type={showPassword ? "text" : "password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <TextField
+          variant="outlined"
+          name="password2"
+          label="Confirm Password"
+          type={showPassword2 ? "text" : "password"}
+          value={password2}
+          onChange={(e) => setPassword2(e.target.value)}
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowPassword2(!showPassword2)} edge="end">
+                  {showPassword2 ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <Button
+          className="mt-6 w-full rounded-lg bg-blue-600 text-white py-3 px-8 shadow-lg text-lg hover:bg-blue-700 transition duration-300 transform hover:scale-105"
+          onClick={() => checkPassword(password, password2, email, name, contactNumber)}
+        >
+          Register
+        </Button>
 
 
-export default function Booking () {
-    const router = useRouter()
-    const dispatch = useDispatch<AppDispatch>()
-
-    const [name, setName] = useState("");
-    const [contactNumber, setContactNumber] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [showPassword, setShowPassword] = useState(false);
-    const [password2, setPassword2] = useState("");
-    const [showPassword2, setShowPassword2] = useState(false);
-
-    const validateEmail = (email: string) => {
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return regex.test(email);
-      };
-
-      const validatePhoneNumber = (phone: string) => {
-        const regex = /^0\d{9}$/;
-        return regex.test(phone);
-      };
-      
-
-      const checkPassword = (
-        password1: string,
-        password2: string,
-        email: string,
-        name: string,
-        contactNumber: string
-      ) => {
-
-        if (!validatePhoneNumber(contactNumber)) {
-            alert("Please enter a valid phone number (10 digits and starts with 0)");
-            return false;
-          }
-          if (!validateEmail(email)) {
-            alert("Please enter a valid email address");
-            return false;
-          }
-      
-        if (password1 !== password2) {
-          alert("Please check your password again");
-          return false;
-        }
-      
-        userRegister(email, password2, name, contactNumber);
-        router.push("/");
-        return true;
-      };
-      
-
-    return (
-        <main className="w-full flex flex-col items-center space-y-6 py-10">
-            <div className="text-2xl font-bold text-stone-950">Register your Account</div>
-            
-            <div className="space-y-6 p-6 flex flex-col max-w-md mx-auto shadow-lg rounded-lg">
-                <TextField variant="standard" name="Name-Lastname" label="Name-Lastname" 
-                value={name} onChange={(e) => {setName(e.target.value) }}/>
-                <TextField variant="standard" name="Contact-Number" label="Contact-Number"
-                value={contactNumber} onChange={(e) => {setContactNumber(e.target.value)}}/>
-
-                <TextField variant="standard" name="email" label="Email"
-                value={email} onChange={(e) => {setEmail(e.target.value)}}/>
-
-                <TextField
-                variant="standard"
-                name="password"
-                label="Password"
-                type={showPassword ? 'text' : 'password'}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                InputProps={{
-                    endAdornment: (
-                    <InputAdornment position="end">
-                        <IconButton
-                        onClick={() => setShowPassword(!showPassword)}
-                        edge="end"
-                        >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                    </InputAdornment>
-                    ),
-                }}
-                />
-
-                <TextField
-                    variant="standard"
-                    name="password2"
-                    label="Confirm Password"
-                    type={showPassword2 ? 'text' : 'password'}
-                    value={password2}
-                    onChange={(e) => setPassword2(e.target.value)}
-                    InputProps={{
-                        endAdornment: (
-                        <InputAdornment position="end">
-                            <IconButton
-                            onClick={() => setShowPassword2(!showPassword2)}
-                            edge="end"
-                            >
-                            {showPassword2 ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>
-                        ),
-                    }}
-                    />
-
-                
-
-                
-                <Button name="register" className="block rounded-md bg-sky-600 hover:bg-indigo-600 
-                px-3 py-2 shadow-sm text-white"
-                onClick={() => {console.log("register") 
-                ; 
-                checkPassword(password , password2 ,email , name ,contactNumber)}}>
-                 Register
-                </Button>
-
-            </div>
-            
-        </main>
-    );
+      </div>
+    </main>
+  );
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,30 +1,67 @@
-'use client'
-import styles from './banner.module.css'
-import Image from 'next/image';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
+"use client";
 
-export default function Banner(){
-    const covers = ['/img/banner02.jpg' , '/img/banner05.jpg' , '/img/banner03.jpg','/img/banner04.jpg']
-    const [index , setIndex] = useState(0) 
-    const rounter = useRouter()
-    const {data : session} = useSession()
-    console.log(session?.user.token)
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import styles from "./banner.module.css";
+
+export default function Banner() {
+    const covers = ["/img/cover3.jpg", "/img/cover4.jpg", "/img/cover2.jpg", "/img/cover.jpg"];
+    const [index, setIndex] = useState(0);
+    const router = useRouter();
+    const { data: session } = useSession();
+
+    const nextIndex = (index + 1) % covers.length;
+    const handleBannerClick = () => setIndex(nextIndex);
+
     return (
-        <div className={styles.banner} onClick={() => setIndex(index+1)}>
-            <Image src={covers[index%4]}
-            alt ='cover'
-            fill={true}
-            priority
-            objectFit='cover' />
-            <div className={styles.bannerText}>
-                <h1 className='text-4xl font-meduim'>where every event finds its venue</h1>
-                <h3 className='text-xl font-serif'>Finding the perfect venue has never been easier. Whether it's a wedding, corporate event, or private party, we connecting people to the perfect place.</h3>
+        <div 
+            className={`${styles.banner} relative h-[400px] w-full overflow-hidden cursor-pointer mt-[60px]`}  // Add margin-top to avoid overlap
+            onClick={handleBannerClick}
+            aria-label="Click to change banner image"
+        >
+            {/* Background Image */}
+            <div className="absolute inset-0">
+                <Image 
+                    src={covers[index % 4]} 
+                    alt="cover" 
+                    fill={true} 
+                    priority 
+                    style={{ objectFit: "cover" }} 
+                    className="transition-opacity duration-700 ease-in-out"
+                    blurDataURL={covers[index % 4]}  // Optional: low-quality placeholder
+                />
+                {/* Gradient Overlay */}
+                <div className="absolute inset-0 bg-gradient-to-r from-black/40 via-black/20 to-transparent"></div>
             </div>
-            {
-                session? <div className='z-30 absolute top-5 right-10 font-semibold text-white text-xl'>Welcome {session.user?.name}</div>:null
-            }
+
+            {/* Text Content */}
+            <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white px-6">
+                <h1 className="text-4xl md:text-5xl font-extrabold tracking-wide drop-shadow-lg" aria-label="Register Now for the Online Job Fair">
+                    🌟 Register Now for the <br /> Online Job Fair & Kickstart Your Career! 🚀
+                </h1>
+            </div>
+
+            {/* Welcome Message */}
+            {session && (
+                <div className="absolute top-6 right-10 bg-black/60 px-4 py-2 rounded-lg text-lg font-semibold text-white shadow-md">
+                    👋 Welcome, {session.user?.name}!
+                </div>
+            )}
+
+            {/* CTA Button */}
+            <button 
+                className="absolute bottom-6 right-10 bg-cyan-600 text-white font-semibold py-3 px-6 rounded-lg shadow-md transition-all 
+                hover:bg-cyan-400 hover:scale-105 focus:ring-4 focus:ring-cyan-300"
+                aria-label="Go to the venue selection page"
+                onClick={(e) => {
+                    e.stopPropagation();
+                    router.push("/venue");
+                }}
+            >
+                🎯 Select Company
+            </button>
         </div>
-    )
+    );
 }
