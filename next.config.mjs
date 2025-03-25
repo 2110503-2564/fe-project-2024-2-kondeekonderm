5fcc0de18d2daa756c3f21a28ab982875a6252de
MasterNay <155442296+MasterNay@users.noreply.github.com>
2025-03-25 23:21:45 +0700

    change vercel

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
     images: {
         domains: ['drive.google.com']
     },
+    experimental: {
+        serverActions: true
+        
+    },
+    env: {
+        FRONTEND_URL: process.env.FRONTEND_URL,
+        BACKEND_URL: process.env.BACKEND_URL 
+    },
     async headers() {
         return [
             {
